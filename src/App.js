35b0d915578049f4717c87af636865339e53a995
Fahim Rimon <fahimrimon21@gmail.com>
2022-11-09 17:56:27 +0600

    cart section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import CategoryPages from "./pages/CategoryPages";
+import Announce from "./components/Announce";
+import Products from "./components/Product/Products";
+import Slider from "./components/Slider/Slider";
+import Navbar from "./Shared/Navbar";
+import NewsLetter from "./components/NewsLetter/NewsLetter";
+import Footer from "./Shared/Footer/Footer";
+import Categories from "./components/Category/Categories";
 
 function App() {
   return (
     <div>
-      <CategoryPages></CategoryPages>
+      <Announce></Announce>
+      <Navbar></Navbar>
+      <Slider></Slider>
+      <Categories></Categories>
+      <Products></Products>
+      <NewsLetter></NewsLetter>
+      <Footer></Footer>
     </div>
   );
 }
--- src/Shared/Navbar.jsx
+++ src/Shared/Navbar.jsx
@@ -8,7 +8,12 @@ const Navbar = () => {
   return (
     <div className="navbar h-[60px] shadow-md relative z-10 ">
       <div className="wrapper lg:pl-[20px] lg:pr-[20px] pt-[10px] pb-[10px] flex justify-between items-center pl-2 pr-2">
-        <div className=" left flex flex-1 items-center">
+        {/* Logo */}
+        <div className="left flex flex-1 ">
+          <div className="logo font-bold text-xl mobile:text-sm">Dress Wear</div>
+        </div>
+
+        <div className=" center flex flex-1 text-center mobile:ml-6 justify-center items-center">
           <div className="cursor-pointer text-[16px] mobile:hidden">En</div>
 
           {/* Search Input */}
@@ -16,11 +21,6 @@ const Navbar = () => {
             <input type="text" className="outline-none mobile:w-[50px]" placeholder="Search" />
             <Search className="text-[#8a4af3]" style={{ fontSize: "16px" }} />
           </div>
-        </div>
-
-        {/* Logo */}
-        <div className="center flex-1 text-center  mobile:ml-6">
-          <div className="logo font-bold mobile:text-sm">Winter Kings</div>
         </div>
 
         {/* Right Side */}
